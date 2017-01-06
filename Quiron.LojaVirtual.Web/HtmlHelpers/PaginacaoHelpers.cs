d510ae9780af1d5d32a648a78d1972d69bcfbf4f
Danielle Ramos <nielleramos@hotmail.com>
2017-01-06 13:16:15 -0200

    Aula 8

---
+++ Quiron.LojaVirtual.Web/HtmlHelpers/PaginacaoHelpers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Quiron.LojaVirtual.Web.Models;
+using System.Text;
+
+namespace Quiron.LojaVirtual.Web.HtmlHelpers
+{
+   
+        public static class PaginacaoHelpers
+        {
+            public static MvcHtmlString PageLink(this HtmlHelpers html, Paginacao paginacao, Func<int, string> paginaUrl)
+            {
+            StringBuilder resultado = new StringBuilder();
+
+            }
+        }
+    
+}
