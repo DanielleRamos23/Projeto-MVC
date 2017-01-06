0ff0ed48345e3f2090f37c47b63e7096daae63b8
Danielle Ramos <nielleramos@hotmail.com>
2017-01-06 14:59:35 -0200

    Concertar Paginação

--- Quiron.LojaVirtual.Web/HtmlHelpers/PaginacaoHelpers.cs
+++ Quiron.LojaVirtual.Web/HtmlHelpers/PaginacaoHelpers.cs
@@ -4,18 +4,16 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Quiron.LojaVirtual.Web.Models;
-using System.Text;
+
 
 namespace Quiron.LojaVirtual.Web.HtmlHelpers
 {
-   
-        public static class PaginacaoHelpers
-        {
-            public static MvcHtmlString PageLink(this HtmlHelpers  html, Paginacao paginacao, Func<int, string> paginaUrl)
-            {
-            StringBuilder resultado = new StringBuilder();
+    public static class PaginacaoHelpers
 
-            }
+    {
+        public static MvcHtmlString PageLinks(this HtmlHelper Html, Paginacao paginacao, Func<int, string> paginaUrl)
+        {
+
         }
-    
+    }
 }
