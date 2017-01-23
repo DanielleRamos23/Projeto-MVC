6d3647e445e8a6e660c54b6c66cb4b9e16d62af7
Danielle Ramos <nielleramos@hotmail.com>
2017-01-23 11:33:32 -0200

    Pginação

--- Quiron.LojaVirtual.Web/HtmlHelpers/PaginacaoHelpers.cs
+++ Quiron.LojaVirtual.Web/HtmlHelpers/PaginacaoHelpers.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Text;
 using System.Web.Mvc;
 using Quiron.LojaVirtual.Web.Models;
 
@@ -13,7 +14,29 @@ namespace Quiron.LojaVirtual.Web.HtmlHelpers
     {
         public static MvcHtmlString PageLinks(this HtmlHelper Html, Paginacao paginacao, Func<int, string> paginaUrl)
         {
+            StringBuilder resultado = new StringBuilder();
 
+            for (int i = 1; i <paginacao.TotalPagina; i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+
+                tag.MergeAttribute("href", paginaUrl(i));
+
+                tag.InnerHtml = i.ToString();
+
+                if(i == paginacao.PaginaAtual)
+                {
+                    tag.AddCssClass("selected");
+                    tag.AddCssClass("btn-primary");
+                    
+                }
+
+                tag.AddCssClass("btn btn-default");
+                resultado.Append(tag);
+
+            }
+
+            return MvcHtmlString.Create(resultado.ToString());
         }
     }
 }
